ea25ee673e0d295f0cb578ef5bec32d62bdb9a2d
theor <theo.richart@gmail.com>
2019-03-01 01:35:06 -0500

    ortho

--- src/main.rs
+++ src/main.rs
@@ -151,44 +151,19 @@ impl<'a, 'b, R: gfx::Resources> gfx_app::Application<R> for App<'a, 'b, R> {
 
         let vertex_data = [
             // top (0, 0, 1)
-            Vertex::new([-1, -1, 1], [0, 0]),
-            Vertex::new([1, -1, 1], [1, 0]),
-            Vertex::new([1, 1, 1], [1, 1]),
-            Vertex::new([-1, 1, 1], [0, 1]),
-            // bottom (0, 0, -1)
-            Vertex::new([-1, 1, -1], [1, 0]),
-            Vertex::new([1, 1, -1], [0, 0]),
-            Vertex::new([1, -1, -1], [0, 1]),
-            Vertex::new([-1, -1, -1], [1, 1]),
-            // right (1, 0, 0)
-            Vertex::new([1, -1, -1], [0, 0]),
-            Vertex::new([1, 1, -1], [1, 0]),
-            Vertex::new([1, 1, 1], [1, 1]),
-            Vertex::new([1, -1, 1], [0, 1]),
-            // left (-1, 0, 0)
-            Vertex::new([-1, -1, 1], [1, 0]),
-            Vertex::new([-1, 1, 1], [0, 0]),
-            Vertex::new([-1, 1, -1], [0, 1]),
-            Vertex::new([-1, -1, -1], [1, 1]),
-            // front (0, 1, 0)
-            Vertex::new([1, 1, -1], [1, 0]),
-            Vertex::new([-1, 1, -1], [0, 0]),
-            Vertex::new([-1, 1, 1], [0, 1]),
-            Vertex::new([1, 1, 1], [1, 1]),
-            // back (0, -1, 0)
-            Vertex::new([1, -1, 1], [0, 0]),
-            Vertex::new([-1, -1, 1], [1, 0]),
-            Vertex::new([-1, -1, -1], [1, 1]),
-            Vertex::new([1, -1, -1], [0, 1]),
+            Vertex::new([-1, -1, 0], [0, 0]),
+            Vertex::new([1, -1, 0], [1, 0]),
+            Vertex::new([1, 1, 0], [1, 1]),
+            Vertex::new([-1, 1, 0], [0, 1]),
         ];
 
         let index_data: &[u16] = &[
             0, 1, 2, 2, 3, 0, // top
-            4, 5, 6, 6, 7, 4, // bottom
-            8, 9, 10, 10, 11, 8, // right
-            12, 13, 14, 14, 15, 12, // left
-            16, 17, 18, 18, 19, 16, // front
-            20, 21, 22, 22, 23, 20, // back
+            // 4, 5, 6, 6, 7, 4, // bottom
+            // 8, 9, 10, 10, 11, 8, // right
+            // 12, 13, 14, 14, 15, 12, // left
+            // 16, 17, 18, 18, 19, 16, // front
+            // 20, 21, 22, 22, 23, 20, // back
         ];
 
         let (vbuf, slice) = factory.create_vertex_buffer_with_slice(&vertex_data, index_data);
@@ -213,7 +188,7 @@ impl<'a, 'b, R: gfx::Resources> gfx_app::Application<R> for App<'a, 'b, R> {
             )
             .unwrap();
 
-        let proj = cgmath::perspective(Deg(45.0f32), window_targets.aspect_ratio, 1.0, 10.0);
+        let proj = cam(window_targets.aspect_ratio); cgmath::perspective(Deg(45.0f32), window_targets.aspect_ratio, 1.0, 10.0);
 
         let data = pipe::Data {
             vbuf: vbuf,
@@ -264,7 +239,7 @@ impl<'a, 'b, R: gfx::Resources> gfx_app::Application<R> for App<'a, 'b, R> {
         self.bundle.data.out_depth = window_targets.depth;
 
         // In this example the transform is static except for window resizes.
-        let proj = cgmath::perspective(Deg(45.0f32), window_targets.aspect_ratio, 1.0, 10.0);
+        let proj = cam(window_targets.aspect_ratio);// cgmath::perspective(Deg(45.0f32), window_targets.aspect_ratio, 1.0, 10.0);
         self.bundle.data.transform = (proj * default_view()).into();
     }
 }
@@ -276,8 +251,15 @@ pub fn main() {
 
 fn default_view() -> Matrix4<f32> {
     Matrix4::look_at(
-        Point3::new(1.5f32, -5.0, 3.0),
+        Point3::new(0f32, 0f32, 10f32),
         Point3::new(0f32, 0.0, 0.0),
-        Vector3::unit_z(),
+        Vector3::unit_y(),
+    )
+}
+
+fn cam(w: f32) -> Matrix4<f32> {
+    let f = 10f32;
+    cgmath::ortho(
+        0.0f32, w*f, f, 0.0f32, 0.0f32, 10.0f32
     )
 }
